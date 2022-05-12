0fe46d6f4d4d4258b594e6ac1e20cd2f65144129
petretiandrea <petretiandrea@kevopartners.com>
2022-05-12 21:49:54 +0200

    added new background image

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,7 @@
         <v-img class="fill-viewport"
             height="100vh"
             white
-            src="img/background-dashboard.jpg"
+            src="/img/centro1.jpg"
             gradient="to bottom, rgba(0, 0, 0, 0.11) 0%, rgba(0, 0, 0, 0.4) 100%">
             <v-container id="contact-header" fluid class="hidden-sm-and-down">
                 <v-row align="center">
@@ -27,33 +27,8 @@
                     </v-col>
                 </v-row>
             </v-container>
-            <v-container fill-height>
-                <v-row fill-height align="center" justify="center">
-                    <v-content class="text-center white--text">
-                        <v-row align="center" justify="center">
-                            <!-- <v-col class="text-left">
-                                <img src="img/be-aesthetic-fit.png" alt="Be Aesthetic"/>
-                            </v-col> -->
-                            <!-- <v-col class="text-left title">
-                                Ciao, sono Andrea Petreti!
-                            </v-col>
-                            <v-col cols="12">
-                                <v-btn text icon href="https://github.com/petretiandrea" target="_blank"><v-icon >mdi-github</v-icon></v-btn>
-                                <v-btn text icon href="https://www.linkedin.com/in/andrea-petreti/" target="_blank"><v-icon>mdi-linkedin</v-icon></v-btn>
-                                <v-btn text icon href="mailto:petretiandrea@gmail.com" target="_blank"><v-icon>mdi-email</v-icon></v-btn>
-                                <v-btn text icon href="https://profile.codersrank.io/user/petretiandrea" target="_blank">
-                                    <icon-svg data="@/assets/icons/coders-rank.svg" width="24" color="#0000008a"/>
-                                </v-btn>
-                            </v-col>
-                            <v-col cols="12" v-if="$vuetify.breakpoint.smAndDown"> 
-                                <v-btn text outlined color="white" large href="/cv">Il mio curriculum</v-btn>
-                            </v-col> -->
-                        </v-row>
-                    </v-content>
-                </v-row>
-            </v-container>
         </v-img>
-    
+
         <section id="services">
             <section-services/>
         </section>
